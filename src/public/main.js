244933195863f1053b985d0fc5cded6a57aac509
Daniele Irsuti <irsooti+code@gmail.com>
2023-03-02 13:21:00 +0100

    Create proposal

--- src/public/main.js
+++ src/public/main.js
@@ -1,26 +1,84 @@
+const NEW_MESSAGE_LISTENER = 'NEW_MESSAGE_LISTENER';
+const RESET_MESSAGE_LISTENER = 'RESET_MESSAGE_LISTENER';
+const KEY_MESSAGE = 'message';
+
 const App = (() => {
-  window.addEventListener('HELLO', (data) => {
-    console.log({ data });
+  // Listen for new messages and update the DOM
+  listenForNewMessage((data) => {
+    window.document.title = data.detail;
     window.document.querySelector('#root').textContent = data.detail;
   });
 
-  const message_broadcast = (message) => {
-    console.log({ message })
-    window.localStorage.setItem('message', JSON.stringify(message));
-    window.localStorage.removeItem('message');
-  }
-
-  function message_receive(ev) {
-    console.log(ev);
-    var message = JSON.parse(ev.originalEvent.newValue);
-
-    window.dispatchEvent(new CustomEvent('HELLO', { detail: message }));
-
-    // etc.
-  }
+  // Start polling for new messages
+  startPolling();
 
   // Listen for changes to localStorage, it works **across** tabs but not the same tab
-  window.addEventListener('storage', message_receive);
-
-  return { message_broadcast };
+  listenForOtherTabs();
 })();
+
+function fetchApi(abortController) {
+  fetch('/api/', { signal: abortController.signal })
+    .then((r) => r.text())
+    .then(broadcastMessage)
+    .then(dispatchNewMessage)
+    .catch((err) => {
+      // ? important to check if the error is an abort error, or else it will log an error
+      if (err.name === 'AbortError') {
+        console.log('Aborted! This is expected.', err);
+        return;
+      }
+      console.error(err);
+    });
+}
+
+function startPolling() {
+  let abortController = new AbortController();
+  let interval = setInterval(() => fetchApi(abortController), 5000);
+
+  window.addEventListener(RESET_MESSAGE_LISTENER, () => {
+    clearInterval(interval);
+
+    abortController.abort();
+    abortController = new AbortController();
+
+    interval = setInterval(() => fetchApi(abortController), 5000);
+  });
+}
+
+function onMessageReceive(ev) {
+  if (ev.key !== KEY_MESSAGE || ev.newValue === null) return;
+
+  var message = JSON.parse(ev.newValue);
+
+  window.dispatchEvent(
+    new CustomEvent(NEW_MESSAGE_LISTENER, { detail: message })
+  );
+  window.dispatchEvent(new CustomEvent(RESET_MESSAGE_LISTENER));
+}
+
+function broadcastMessage(message) {
+  window.localStorage.setItem(KEY_MESSAGE, JSON.stringify(message));
+  window.localStorage.removeItem(KEY_MESSAGE);
+  return message;
+}
+
+function dispatchNewMessage(data) {
+  return dispatchNewMessage(data);
+}
+
+function dispatchResetPolling() {
+  window.dispatchEvent(new CustomEvent(RESET_MESSAGE_LISTENER));
+}
+
+function dispatchNewMessage(data) {
+  window.dispatchEvent(new CustomEvent(NEW_MESSAGE_LISTENER, { detail: data }));
+  return data;
+}
+
+function listenForOtherTabs() {
+  window.addEventListener('storage', onMessageReceive);
+}
+
+function listenForNewMessage(callback) {
+  window.addEventListener(NEW_MESSAGE_LISTENER, callback);
+}
